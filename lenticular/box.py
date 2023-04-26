0ada51ca133187ac5debf905c07e0b3064ebcfcd
apjanco <apjanco@gmail.com>
2023-04-26 17:23:50 -0400

    end of day

--- lenticular/box.py
+++ lenticular/box.py
@@ -1,17 +1,69 @@
+import os
 import requests 
+import yaml
+from pathlib import Path
 from boxsdk import Client, OAuth2
 
-#Go to
+# Go to
 #https://developer.box.com/guides/cli/quick-start//
 #https://developer.box.com/guides/cli/quick-start/create-oauth-app/
+# note that developer token is only valid for 60 minutes
+BOX_CLIENT_ID = os.environ.get('BOX_CLIENT_ID')
+BOX_CLIENT_SECRET = os.environ.get('BOX_CLIENT_SECRET')
+BOX_DEVELOPER_TOKEN = os.environ.get('BOX_DEVELOPER_TOKEN')
 
-auth = OAuth2(
-    client_id='YOUR_CLIENT_ID',
-    client_secret='YOUR_CLIENT_SECRET',
-    access_token='YOUR_DEVELOPER_TOKEN',
-)
-client = Client(auth)
 
-f = client.file('1200022205243').get()
-url = f.get_download_url()
-file = requests.get(url)
+
+def read_policies():
+    with open('policies.yaml', 'r') as file:
+        policies = yaml.safe_load(file)
+    return policies
+
+def get_box_client():
+    if BOX_CLIENT_ID and BOX_CLIENT_SECRET and BOX_DEVELOPER_TOKEN:
+        auth = OAuth2(
+            client_id=BOX_CLIENT_ID,
+            client_secret=BOX_CLIENT_SECRET,
+            access_token=BOX_DEVELOPER_TOKEN,
+        )
+        return Client(auth)
+    else:
+        print("Please set BOX_CLIENT_ID, BOX_CLIENT_SECRET, and BOX_DEVELOPER_TOKEN environment variables")
+
+
+class Box:
+    """
+    Instantiate a Box session.
+    
+    """
+
+    def __init__(self):
+        self.client = get_box_client()
+        self.policies = read_policies()
+
+    
+    def folder_contents(self, folder_id:str):
+        """Return all files in a folder and its subfolders"""
+        contents = []
+        folder = self.client.folder(folder_id).get_items()
+        for item in folder:
+            if item.type == 'folder':
+                self.folder_contents(item.id)
+            elif item.type == 'file':
+                contents.append({'name':item.name, 'id':item.id, 'download_url':item.get_download_url()})
+            print(f'{item.type.capitalize()} {item.id} is named "{item.name}"')
+    
+    def search(self, query:str):
+        """Search for files in box"""
+        return self.client.search().query(query=query).get()
+    
+    def download_file(self, file_id:str, path: str = None):
+        """Download a file from box"""
+        metadata = self.client.file(file_id=file_id).get_all_metadata()
+        print(f'Got metadata instance {metadata["$id"]}')
+        f = self.client.file(file_id).get()
+        url = f.get_download_url()
+        file = requests.get(url)
+        Path(path + f.name).with_suffix(suffix).write_bytes(file.content)
+
+    
--- lenticular/huggingface/dataset.py
+++ lenticular/huggingface/dataset.py
@@ -1,3 +1,4 @@
+import srsly
 from pathlib import Path
 from datasets import load_dataset
 
@@ -6,12 +7,54 @@ def gen(file_path:Path):
         if file.is_file():
             yield {"path": str(file), "name": file.name}
     
+def create_metadata(project_path:Path):
+    metadata = {}
+    srsly.write_jsonl((project_path / 'metadata.jsonl'),metadata)
 
 def create_dataset(file_path:Path, dataset_name:str = 'dataset'):
     #TODO, how to work with all file formats? Also how does this work?
     #https://huggingface.co/docs/datasets/package_reference/main_classes
     ds = load_dataset("imagefolder", data_dir=file_path)
+    dataset = load_dataset("json", data_files="datasets-issues.jsonl", split="train")
     # a = gen(file_path)
     # ds = Dataset.from_generator(a)
     # ds.save_to_disk(dataset_name)
     return ds
+
+#1 add file binary to dataset, currently only works with images and audio, but we can make our own
+
+# existing image https://sourcegraph.com/github.com/huggingface/datasets/-/blob/src/datasets/packaged_modules/imagefolder/imagefolder.py
+from datasets.packaged_modules.folder_based_builder import folder_based_builder
+
+class FolderBuilderConfig(folder_based_builder.FolderBasedBuilderConfig):
+    """BuilderConfig for Folder."""
+
+    drop_labels: bool = None
+    drop_metadata: bool = None
+
+
+class FolderBuilder(folder_based_builder.FolderBasedBuilder):
+    """
+    BASE_FEATURE: feature object to decode data (i.e. datasets.Image(), datasets.Audio(), ...)
+    BASE_COLUMN_NAME: string key name of a base feature (i.e. "image", "audio", ...)
+    BUILDER_CONFIG_CLASS: builder config inherited from `folder_based_builder.FolderBasedBuilderConfig`
+    EXTENSIONS: list of allowed extensions (only files with these extensions and METADATA_FILENAME files
+            will be included in a dataset)
+    CLASSIFICATION_TASK: classification task to use if labels are obtained from the folder structure
+    https://sourcegraph.com/github.com/huggingface/datasets/-/blob/src/datasets/packaged_modules/folder_based_builder/folder_based_builder.py
+    """
+    
+    BUILDER_CONFIG_CLASS = FolderBuilderConfig
+    EXTENSIONS = ["jpg", "jpeg", "png", "bmp", "wav", "mp3", "flac", "txt"]
+    BASE_FEATURE = None #TODO does this allow multiple base features? Only image and audio features currently supported
+    """
+    import pyarrow as pa
+    pa_type: ClassVar[Any] = pa.struct({"bytes": pa.binary(), "path": pa.string()})
+    """
+    
+    BASE_COLUMN_NAME = None
+    METADATA_FILENAME = "metadata.jsonl"
+
+
+#2 use Repository class to push to hub https://huggingface.co/docs/huggingface_hub/v0.8.1/en/how-to-upstream
+
